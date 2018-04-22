e44493ea9135786c0d97eeedca6be52f1f5cd446
Andrzej Sobel <andrzej.sobel@gmail.com>
2018-04-22 19:40:55 +0200

    refaktoryzacja

--- src/elpredatoro/permutacje/Main.java
+++ src/elpredatoro/permutacje/Main.java
@@ -16,24 +16,5 @@ public class Main {
 				new MainFrame();
 			}
 		});
-		
-		/*
-		Dictionary dc = new Dictionary();
-		try {
-			String str = "oko";
-			ArrayList<Character> chars = new ArrayList<Character>();
-			for (char c : str.toCharArray()) {
-				chars.add(c);
-			}
-			
-			ArrayList<String> lista = dc.findMatchingWords(4, chars);
-			System.out.print(lista);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		*/
-
 	}
-
 }
