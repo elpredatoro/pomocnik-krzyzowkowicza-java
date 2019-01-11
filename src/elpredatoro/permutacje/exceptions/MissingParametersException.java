80ddcc55b24357eefa5a9434a323ed2485edf236
Andrzej Sobel <andrzej.sobel@pronet.com.pl>
2019-01-11 14:07:18 +0100

    added exceptions

---
+++ src/elpredatoro/permutacje/exceptions/MissingParametersException.java
@@ -0,0 +1,14 @@
+package elpredatoro.permutacje.exceptions;
+
+public class MissingParametersException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4119134347787185259L;
+
+	public MissingParametersException(String message) {
+		super(message);
+	}
+	
+}
---
+++ src/elpredatoro/permutacje/exceptions/TooManyInputLettersException.java
@@ -0,0 +1,14 @@
+package elpredatoro.permutacje.exceptions;
+
+public class TooManyInputLettersException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5635419360404308323L;
+
+	public TooManyInputLettersException(String message) {
+		super(message);
+	}
+	
+}
