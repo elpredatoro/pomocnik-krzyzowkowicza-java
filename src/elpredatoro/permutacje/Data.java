79927ef3043b20ff556c6d0989c853f690a34b5f
Andrzej Sobel <andrzej.sobel@pronet.com.pl>
2020-01-10 10:37:05 +0100

    podniesienie numeru wersji

--- src/elpredatoro/permutacje/Data.java
+++ src/elpredatoro/permutacje/Data.java
@@ -3,7 +3,7 @@ package elpredatoro.permutacje;
 public class Data {
 	private String version;
 	private Integer majorVersion = 0;
-	private Integer minorVersion = 4;
+	private Integer minorVersion = 5;
 	private String authorName = "Andrzej Sobel";
 	private String authorNick = "elpredatoro";
 	
